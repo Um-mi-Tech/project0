cdab0f514a826289688a54621d019dc3fa33cf1d
Um-mi-Tech <Ummuhlikuthum>
2024-03-28 11:17:04 -0800

    all update in repository

--- .js
+++ .js
@@ -1,4 +1,18 @@
 
- console.log("Hello world");
+/* console.log("Hello world");
   prompt("Enter your name");
-   console.log("Hello" + yourname);
+  x = "Ummuhlikuthum";
+  console.log("Hello" + x );
+   let x = 4, y = 6;
+   console.log(x+y)*/
+   
+  for(let line ="#"; line.length < 8;line+= "#")
+  console.log(line);
+for(let n =1; n <= 100; n++) {
+    let output=" ";
+    if (n %3 ==0)output+="fizz";
+    if (n %5 ==0)output+=" buzz";
+    console.log(output || n);
+    
+
+}
